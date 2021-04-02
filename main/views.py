1a506cdcce8c74ab52f54edce01b0db121bb60ac
rose-okoth <okoth.rose0@gmail.com>
2021-04-02 10:29:12 +0300

    added templates for base, landing page and new project page

--- main/views.py
+++ main/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from urllib.parse import quote_plus
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.db.models import Q
+from django.utils import timezone
+from django.contrib import messages
+from .models import Post
+from .forms import PostForm, RegistrationForm
+from django.contrib.auth import authenticate, logout,login
+from .email import send_welcome_email
+import datetime
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def welcome(request):
     return HttpResponse('This is the landing page!')
+
+def create

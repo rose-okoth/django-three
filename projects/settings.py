b15eb4e59b7e5d276f2de197780cc4b6f3ccead0
rose-okoth <okoth.rose0@gmail.com>
2021-04-05 10:56:00 +0300

    installed crispy forms and bootstrap 4

--- projects/settings.py
+++ projects/settings.py
@@ -133,8 +133,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 
 EMAIL_USE_TLS = config('EMAIL_USE_TLS')
 EMAIL_HOST = config('EMAIL_HOST')

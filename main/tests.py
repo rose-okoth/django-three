7e1d937b3eb46f0f4442956839f0f0e6a8cdc750
rose-okoth <okoth.rose0@gmail.com>
2021-04-07 13:38:34 +0300

    added user and project tests

--- main/tests.py
+++ main/tests.py
@@ -1,10 +1,37 @@
 from django.test import TestCase
 from .models import *
 
+class UserTestClass(TestCase):
+
+# Set up method
+    def setUp(self):
+        self.rose= User(username = 'Rose', password = '123456789')
+
+# Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.rose,User))
+
+class ProjectTestClass(TestCase):
+    
+    def setUp(self):
+        # Creating a new user and saving it
+        self.rose= user(first_name = 'Rose', last_name ='Okoth', email ='okoth.rose@gmail.com')
+        self.rose.save_user()
+
+        # Creating a new tag and saving it
+        self.new_slug = slug(name = 'testing')
+        self.new_slug.save()
+        self.new_post= Post(title = 'Test Post',content = 'This is a random test',editor = self.rose)
+        self.new_post.save()
+        self.new_post.slug.add(self.new_slug)
+
+        def tearDown(self):
+            User.objects.all().delete()
+            Post.objects.all().delete()
 
 class TestProfile(TestCase):
     def setUp(self):
-        self.user = User(id=1, username='mimi', password='passw0rd')
+        self.user = User(username='mimi', password='passw0rd')
         self.user.save()
 
     def test_instance(self):
@@ -17,42 +44,40 @@ class TestProfile(TestCase):
         self.user.delete()
 
 
-# class PostTest(TestCase):
+# class ProjectTest(TestCase):
 #     def setUp(self):
-#         self.user = User.objects.create(id=1, username='charles')
-#         self.post = Post.objects.create(id=1, title='test post', photo='https://ucarecdn.com/0ccf61ff-508e-46c6-b713-db51daa6626e', description='desc',
-#                                         user=self.user, url='http://ur.coml')
+#         self.user = User.objects.create(username='mimi')
+#         self.project = Project.objects.create(title='project', image='https://picture.com', description='description', link='http://url.com')
 
 #     def test_instance(self):
-#         self.assertTrue(isinstance(self.post, Post))
+#         self.assertTrue(isinstance(self.project, Project))
 
-#     def test_save_post(self):
-#         self.post.save_post()
-#         post = Post.objects.all()
-#         self.assertTrue(len(post) > 0)
+#     def test_save_project(self):
+#         self.project.save_project()
+#         project = Project.objects.all()
+#         self.assertTrue(len(project) > 0)
 
-#     def test_get_posts(self):
-#         self.post.save()
-#         posts = Post.all_posts()
-#         self.assertTrue(len(posts) > 0)
+#     def test_get_projects(self):
+#         self.project.save()
+#         projects = Project.all_projects()
+#         self.assertTrue(len(projects) > 0)
 
-#     def test_search_post(self):
-#         self.post.save()
-#         post = Post.search_project('test')
-#         self.assertTrue(len(post) > 0)
+#     def test_search_project(self):
+#         self.project.save()
+#         project = Project.search_project('test')
+#         self.assertTrue(len(project) > 0)
 
-#     def test_delete_post(self):
-#         self.post.delete_post()
-#         post = Post.search_project('test')
-#         self.assertTrue(len(post) < 1)
+#     def test_delete_project(self):
+#         self.project.delete_project()
+#         project = Project.search_project('test')
+#         self.assertTrue(len(project) < 1)
 
 
 # class RatingTest(TestCase):
 #     def setUp(self):
-#         self.user = User.objects.create(id=1, username='charles')
-#         self.post = Post.objects.create(id=1, title='test post', photo='https://ucarecdn.com/0ccf61ff-508e-46c6-b713-db51daa6626e', description='desc',
-#                                         user=self.user, url='http://ur.coml')
-#         self.rating = Rating.objects.create(id=1, design=6, usability=7, content=9, user=self.user, post=self.post)
+#         self.user = User.objects.create(username='mimi')
+#         self.project = Project.objects.create(title='project', image='https://picture.com', slug='slug', description='description', link='http://url.com')
+#         self.rating = Rating.objects.create(design=6, usability=7, content=9, user=self.user, project=self.project)
 
 #     def test_instance(self):
 #         self.assertTrue(isinstance(self.rating, Rating))
@@ -62,7 +87,7 @@ class TestProfile(TestCase):
 #         rating = Rating.objects.all()
 #         self.assertTrue(len(rating) > 0)
 
-#     def test_get_post_rating(self, id):
+#     def test_get_project_rating(self, slug=None):
 #         self.rating.save()
-#         rating = Rating.get_ratings(post_id=id)
+#         rating = Rating.get_ratings(project_slug=slug)
 #         self.assertTrue(len(rating) == 1)
